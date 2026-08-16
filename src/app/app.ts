db8fb09b2963fd9bf6bfd74ddf8a4d1f77464a09
bishirkdy <bishirbishi1@gmail.com>
2026-08-16 11:09:36 +0530

    added simple default html and css basic

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
+import { Footer } from './components/footer/footer';
+import { Contact } from './components/contact/contact';
+import { Home } from './components/home/home';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, Home, Contact, Footer],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -6,4 +6,7 @@ import { Component } from '@angular/core';
   templateUrl: './home.html',
   styleUrl: './home.css',
 })
-export class Home {}
+export class Home {
+    title = 'Welcome to My World';
+    description = 'I am Web Developer';
+}
